65a244364d0b0681508197c5ca5a71733e764f9e
katerinadmit <107727106+katerinadmit@users.noreply.github.com>
2022-08-16 00:09:43 +0300

    ---

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,15 +1,31 @@
 (() => {
-  const menuOpenBtn = document.querySelector('[data-menu-open]');
-  const menuCloseBtn = document.querySelector('[data-menu-close]');
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+  };
 
-  const mobileMenu = document.querySelector('[data-menu]');
-  // const body = document.querySelector('body');
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-  menuOpenBtn.addEventListener('click', openModal);
-  menuCloseBtn.addEventListener('click', openModal);
-
-  function openModal() {
-    mobileMenu.classList.toggle('is-open');
-    // body.classList.toggle('no-scroll');
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
   }
 })();
+
+
+// (() => {
+//   const menuOpenBtn = document.querySelector('[data-menu-open]');
+//   const menuCloseBtn = document.querySelector('[data-menu-close]');
+
+//   const mobileMenu = document.querySelector('[data-menu]');
+//   // const body = document.querySelector('body');
+
+//   menuOpenBtn.addEventListener('click', openModal);
+//   menuCloseBtn.addEventListener('click', openModal);
+
+//   function openModal() {
+//     mobileMenu.classList.toggle('is-open');
+//     // body.classList.toggle('no-scroll');
+//   }
+// })();
